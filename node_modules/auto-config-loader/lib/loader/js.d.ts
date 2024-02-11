0279d4e8fa03e73fd6a64272529c72e907675dd0
Lucas Ennouchi <ennoucas@gmail.com>
2024-02-11 22:33:37 +0000

    adding gitignore

--- node_modules/auto-config-loader/lib/loader/js.d.ts
+++
@@ -1,9 +0,0 @@
-import type { JITIOptions } from 'jiti/dist/types';
-import { Options } from 'sucrase';
-export interface LoadConfOption {
-    jiti?: boolean;
-    jitiOptions?: JITIOptions;
-    transformOption?: Options;
-}
-export declare function loadConf<T>(path: string, option?: LoadConfOption): T;
-export declare function jsLoader<T>(filepath: string, content: string, option?: LoadConfOption): T;
